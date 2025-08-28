1c3186314f68313bed025d5950d9ac95504e801c
Priyanshi0503 <priyanshi0057@gmail.com>
2025-08-28 10:19:45 +0530

    Create celcius.c

---
+++ celcius.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int temp;
+printf("enter a temperatue in celsius:");
+scanf("%d", &temp);
+printf("temperature in fahrenheit: %d\n",(temp*9.0 /5.0) +32);
+return 0;
+}

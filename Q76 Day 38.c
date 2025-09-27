5900e011f3430ad9cf7cb4e5bf3fa87817cce938
Priyanshi0503 <priyanshi0057@gmail.com>
2025-09-27 23:14:36 +0530

    100 days of code

---
+++ Q76 Day 38.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Ask user for the size of the square matrix
+    printf("Enter the size of the square matrix: ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    // Input matrix elements
+    printf("Enter elements of the matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check for symmetry
+    int isSymmetric = 1; // assume true
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0; // not symmetric
+                break;
+            }
+        }
+        if(!isSymmetric) {
+            break;
+        }
+    }
+
+    // Display result
+    if(isSymmetric) {
+        printf("The matrix is symmetric.\n");
+    } else {
+        printf("The matrix is not symmetric.\n");
+    }
+
+    return 0;
+}

91802b4f2e06da9d6df020ac0f9db6a9154df060
Priyanshi0503 <priyanshi0057@gmail.com>
2025-10-21 00:05:14 +0530

    Create Q107 day57.c

---
+++ Q107 day57.c
@@ -0,0 +1,45 @@
+// Write a program to take an array arr[] of integers as input, 
+// the task is to find the previous greater element for each element of the array 
+// in order of their appearance in the array. 
+// Previous greater element of an element in the array is the nearest element on the left 
+// which is greater than the current element. 
+// If there does not exist previous greater of current element, 
+// then previous greater element for current element is -1.
+//
+// N.B:
+// - Print the output for each element in a comma separated fashion.
+// - Do not use Stack, use brute force approach (nested loop) to solve.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    // Input size of array
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    // Input elements of array
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Find Previous Greater Element using brute force
+    for (int i = 0; i < n; i++) {
+        int prevGreater = -1;
+        for (int j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[i]) {
+                prevGreater = arr[j];
+                break;
+            }
+        }
+        printf("%d", prevGreater);
+        if (i != n - 1)
+            printf(", ");
+    }
+    
+    return 0;
+}

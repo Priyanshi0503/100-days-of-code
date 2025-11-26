011915cefab3fdee41d5ae00e20545d6b8ca6d8d
Priyanshi0503 <priyanshi0057@gmail.com>
2025-11-27 00:01:10 +0530

    Update Q38 Day19.c

--- Q38 Day19.c
+++ Q38 Day19.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d", &num);
     while (num != 0) {
         sum += num % 10;
-        num /= 10;
+        num /= 10;//
     }
     printf("The sum of the digits is: %d\n", sum);
     return 0;

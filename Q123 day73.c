c31382e59ddababa4c0ab7eda9d9ea6fb865ab21
Priyanshi0503 <priyanshi0057@gmail.com>
2025-11-30 13:56:26 +0530

    Create Q123 day73.c

---
+++ Q123 day73.c
@@ -0,0 +1,46 @@
+/*
+Q123 (File Handling)
+Read a text file and count the total number of characters, words, and lines.
+A word is defined as a sequence of non-space characters separated by 
+spaces or newlines.
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    fp = fopen("info.txt", "r");   // Open the file for reading
+
+    if (fp == NULL) {
+        printf("Error: Could not open file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (!isspace(ch) && inWord == 0) {
+            inWord = 1;
+            words++;
+        } 
+        else if (isspace(ch)) {
+            inWord = 0;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Characters: %d\n", characters);
+    printf("Words     : %d\n", words);
+    printf("Lines     : %d\n", lines);
+
+    return 0;
+}
